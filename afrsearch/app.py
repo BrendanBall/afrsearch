376452f9fd31360dca107676b1e0cd38d0c55f77
wraithy <wraith.andrew@gmail.com>
2015-05-22 10:13:52 +0200

    flask now gets results from solr

--- afrsearch/app.py
+++ afrsearch/app.py
@@ -1,6 +1,11 @@
+import json
+import requests
 from flask import Flask, send_from_directory, render_template, request
 
 app = Flask(__name__)
+
+solr_url = "http://localhost:8983/solr/af-search/select"
+solr_opt = {"wt": "json"}
 
 @app.route("/", methods=["GET"])
 def index():
@@ -8,7 +13,11 @@ def index():
 
 @app.route("/search", methods=["GET"])
 def search():
-	return render_template("index.html", title="Search", searchquery=request.args["searchquery"])
+	solr_opt["q"] = request.args["query"]
+	response = requests.get(solr_url, params=solr_opt)
+	results = json.loads(response.content)
+	return response.content
+	# return render_template("index.html", title="Search", query=request.args["query"])
 
 @app.route("/static/<path:path>", methods=["GET"])
 def staticfiles(path):
